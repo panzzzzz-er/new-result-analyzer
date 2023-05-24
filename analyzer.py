f885610187f79e76bd7b75b7909a5b1f7d91909a
Mohit Pansare <mohitpansare@gmail.com>
2023-05-24 19:06:17 +0530

    first

---
+++ analyzer.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import statistics as stats
+import math
+
+
+def count_students(percentages):
+    student_ranges = {
+        '>60': 0,
+        '(50, 60]': 0,
+        '(40, 50]': 0,
+        '(12, 40]': 0,
+        'Fail/KT': 0
+    }
+
+    for percentage in percentages:
+        if percentage >= 60:
+            student_ranges['>60'] += 1
+        elif 50 < percentage <= 60:
+            student_ranges['(50, 60]'] += 1
+        elif 40 < percentage <= 50:
+            student_ranges['(40, 50]'] += 1
+        elif 12 < percentage <= 40:
+            student_ranges['(12, 40]'] += 1
+        elif percentage <= 12:
+            student_ranges['Fail/KT'] += 1
+
+    return student_ranges
+
+
+def get_avg_cgpa(cleared_result):
+    CGPA = cleared_result['cgpa'].fillna(0).values.tolist()
+    CGPA = [float(i) for i in CGPA]
+    avg_cgpa = stats.mean(CGPA)
+    return avg_cgpa, CGPA
+
+
+def get_avg_percentage(CGPA):
+    percentages = []
+    for i in range(len(CGPA)):
+        if CGPA[i] >= 7:
+            percentages.append(round(7.4*CGPA[i] + 12, 4))
+        elif CGPA[i] < 7:
+            percentages.append(round(7.1*CGPA[i] + 12, 4))
+        elif CGPA[i] == 0.0:
+            percentages.append(0)
+    # print("PERCENTAGE: ", percentages)
+    avg_perc = stats.mean(percentages)
+    return avg_perc, percentages
+
+
+def find_toppers(cleared_result):
+
+    copy_df = cleared_result.copy()
+    copy_df['cgpa'] = pd.to_numeric(copy_df['cgpa'])
+    # find the top 3 students by CGPA
+    top3 = copy_df.nlargest(3, 'cgpa')
+
+    # select only the 'rollno' and 'cgpa' columns
+    top3 = top3.loc[:, ['roll_no', 'cgpa']]
+
+    # convert the result to a dictionary
+    toppers = top3.to_dict('records')
+
+    return toppers
+
+
+def find_sub_max(cleared_result, semester):
+    subjects = pd.read_excel('subjects.xlsx')
+    subs = subjects[semester].tolist()
+    # print(subs)
+    heads = cleared_result.columns
+    columns = [i for i in range(len(heads)) if isinstance(
+        heads[i], int) and heads[i] % 10 == 1]
+    # print(columns)
+    result = {}
+    for col in columns:
+        name = subs[col]
+        # print(name)
+        try:
+            result[name] = cleared_result[heads[col]].dropna().apply(
+                pd.to_numeric, errors='coerce').max()
+        except ValueError:
+            print(f"Column {col} cannot be converted to float.")
+    return result
+
+
+def find_kts(cleared_result):
+    kts = 0
+    for i in cleared_result['cgpa']:
+        if i != i:
+            kts += 1
+    return kts
---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, request, url_for, send_file
+import reform as rf
+import analyzer as ana
+import statistics as stats
+import seaborn as sns
+import pandas as pd
+import io
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['csv'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+UPLOAD_FOLDER = os.path.join('static', 'upload')
+
+# app = Flask(__name__)'
+
+
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    @app.route("/")
+    def index():
+        return render_template('home.html')
+
+    @app.route('/download')
+    def download():
+        filename = request.args.get('filename')
+        return send_file(f'processed_csv\\{filename}', as_attachment=True)
+
+    @app.route("/report", methods=['POST', 'GET'])
+    def report():
+        semester = int(request.form.get('semester'))
+        br = request.form.get('branch')
+        year = request.form.get('year')
+        try:
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                data_filename = secure_filename(file.filename)
+                file.save(os.path.join('upload', data_filename))
+                newname = f'upload/{data_filename}'
+
+        except Exception as e:
+            print(e)
+
+        branch = ''
+        if br=='a': branch = 'Artificial Intelligence and Data Science'
+        elif br=='c': branch = 'Computer Science Engineering'
+        elif br=='e': branch = 'Electronics and Computer Science'
+        elif br=='m': branch = 'Mechanical Engineering'
+
+        result = pd.read_csv(newname)
+        cleared_result = rf.clear_result_data(result, semester)
+        # print(cleared_result)
+
+        strenght = len(cleared_result)
+
+        # to allow user to download the cleaned csv file
+        filename = 'cleaned_mu_result.csv'
+        path = os.path.join('processed_csv', filename)
+        cleared_result.to_csv(path, index=False)
+        download_link = f'/download?filename={filename}'
+
+        avg_cgpa, CGPA = ana.get_avg_cgpa(cleared_result)
+        avg_perc, percentages = ana.get_avg_percentage(CGPA)
+        # print(avg_cgpa, avg_perc)
+        # print(CGPA)
+        # print(percentages)
+
+        toppers = ana.find_toppers(cleared_result)
+        # print(toppers)
+
+        ten_cgpaers = CGPA.count(10)
+        subject_maxmarks = ana.find_sub_max(cleared_result, semester)
+        # print(ten_cgpaers)
+        # print(subject_maxmarks)
+
+        plot1 = sns.displot(CGPA, kde=True, bins=15)
+        plot1.savefig('static/graphs/cgpa.png')
+
+        plot2 = sns.displot(percentages, kde=True, bins=15)
+        plot2.savefig('static/graphs/perc.png')
+
+        student_ranges = ana.count_students(percentages)
+        # print(student_ranges)
+
+        kts = ana.find_kts(cleared_result)
+        # print(kts)
+
+        return render_template('report.html',
+                               semester=semester,
+                               branch=branch,
+                               year=year,
+                               filename=filename,
+                               download_link=download_link,
+                               avg_cgpa=round(avg_cgpa, 3),
+                               avg_perc=round(avg_perc, 3),
+                               toppers=toppers,
+                               ten_cgpaers=ten_cgpaers,
+                               subject_maxmarks=subject_maxmarks,
+                               student_ranges=student_ranges,
+                               kts=kts,
+                               strenght = strenght
+                               )
+
+    if __name__ == '__main__':
+        app.run(debug=True, port='5000')
+
+    return app
